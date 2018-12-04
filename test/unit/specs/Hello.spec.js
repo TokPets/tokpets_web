9f5c300121119705812e5fc844323d847653c3fa
jorgemayorga@bitbucket.org <jorgemayorga@bitbucket.org>
2018-12-04 00:32:45 -0500

    DeployUpdate

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -5,7 +5,6 @@ describe('vLoader.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(vLoaderComponent)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js PWA')
+    expect(vm.$el.querySelector('.hello h1').textContent).to.equal('Welcome to Your Vue.js PWA')
   })
 })
